c67e7c7abd40e2a9fc23e07e81d711cf2283ed11
NAWAAL HANIF MUMTAZ ARRIYE <nawaalhanif@gmail.com>
2026-03-08 11:20:12 +0700

    Selesai Praktikum 03: Implementasi Relasi Antar Objek

---
+++ Praktikum 03/In-Lab/src/MataKuliah.java
@@ -0,0 +1,22 @@
+public class MataKuliah {
+    private String idMatKul, nama;
+    private int sks;
+
+    public MataKuliah() { }
+
+    public MataKuliah(String idMatKul, String nama, int sks) {
+        this.idMatKul = idMatKul;
+        this.nama = nama;
+        this.sks = sks;
+    }
+
+
+    public String getIdMatKul() { return idMatKul; }
+    public String getNama() { return nama; }
+    public int getSks() { return sks; }
+
+
+    public void setIdMatKul(String idMatKul) { this.idMatKul = idMatKul; }
+    public void setNama(String nama) { this.nama = nama; }
+    public void setSks(int sks) { this.sks = sks; }
+}
